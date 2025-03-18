61d5eb98772939050afba82e793a687e310b932d
Max Edison <amirhossein.heidari.email@gmail.com>
2025-03-19 03:03:06 +0330

    setup logging

---
+++ bot.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logger.info("Bot starting...")
